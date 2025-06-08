42141bc6308173ddacdd3f306daf765feddac100
figuemadix <mariana.figueiredo@radixeng.com.br>
2025-06-08 13:52:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function changeLanguage() {
 const lang = document.getElementById("language-select").value;
 
 // Update headers
-//document.querySelector("label[for='language-select']").textContent = translations[lang].language; 
+document.querySelector("label[for='language-select']").textContent = translations[lang].language; 
 document.querySelector("h1").textContent = translations[lang].header_title;
 document.querySelectorAll(".card > h2")[0].textContent = translations[lang].impact;
 document.querySelectorAll(".card > h2")[1].textContent = translations[lang].urgency;
